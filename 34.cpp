2549c72cda7d3477814f3d3167925fadac489257
FuadAhmad82 <lsfuad@hotmail.com>
2019-04-07 01:41:53 +0300

    Create 34.cpp

---
+++ 34.cpp
@@ -0,0 +1,20 @@
+// Example program
+#include <iostream>
+#include <string>
+
+int main()
+{
+	int favorite_number;
+	std::cout << "Enter your Favorite Number";
+	
+	std::cin >> favorite_number;
+
+	std::cout << "this is my number too " ;
+    std::cout << favorite_number ;
+
+	std::cout << " this is no  " << std::endl;
+
+    
+
+	return 0;
+}
