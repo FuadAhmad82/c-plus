3ec418fb3ed38143e710592edb4072486944524a
FuadAhmad82 <lsfuad@hotmail.com>
2019-04-07 23:12:32 +0300

    Create 42-1.cpp

---
+++ 42-1.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+	int num1; 
+	double num2;
+
+
+	std::cout << "Enter Your Integer"<< std::endl;
+	std::cin >> num1 >> num2; 
+	std::cout << "Yout integer is " << num1 << " And Your double is " << num2 << std::endl;
+ 	system("pause");
+	return 0;
+		
+}
+
+ 
