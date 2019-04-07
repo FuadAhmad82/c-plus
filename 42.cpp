9e2afe55f1f6aa1f98c7bac90596f8238e906f3f
FuadAhmad82 <lsfuad@hotmail.com>
2019-04-07 23:06:36 +0300

    Create 42.cpp

---
+++ 42.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+	int num1;
+	int num2;
+	std::cout << "Enter First Number \n";
+	std::cin >> num1;
+	std::cout << "Enter Second Number \n";
+	std::cin >> num2;
+	std::cout << "You enter Number " << num1 << " And Number " << num2 <<"\n"; 
+	std::cout << num1 + num2 << "\n"; 
+	system("pause");
+	return 0;
+}
+
+ 
