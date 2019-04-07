348436aaa6fef8534b567d4166d69626f1546f3c
FuadAhmad82 <lsfuad@hotmail.com>
2019-04-07 10:40:38 +0300

    Create 41.cpp

---
+++ 41.cpp
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;  // use this to remove any std from the code .
+
+
+int _tmain()
+{
+	int favorite_number;
+	cout << "Enter your Favorite Number Between 1 and 100 ";
+	cin >> favorite_number;
+	cout << "Amazing that's my favorite number too" << endl;
+	cout << "No really " << favorite_number << " is my favorite number" << endl;
+	system("pause"); // pause the command line 
+	return 0;
+}
+
