158cdd20451c6169327467f21a66ee6f39997541
FuadAhmad82 <lsfuad@hotmail.com>
2019-04-10 10:02:18 +0300

    Create 45.cpp

---
+++ 45.cpp
@@ -0,0 +1,24 @@
+#include "stdafx.h"
+#include <iostream>
+using namespace std; 
+
+
+int main()
+{
+
+
+	cout << "Please Enter height" << endl;
+	int room_height;
+	cin >> room_height;
+
+	cout << "Please Weight height" << endl;
+	int room_weight;
+	cin >> room_weight;
+
+	cout << room_height * room_weight << endl;
+
+	return 0;
+
+
+}
+
