af99462b05b1326aec9a885e25a4f67c04d4ab57
FuadAhmad82 <lsfuad@hotmail.com>
2019-04-13 01:26:53 +0300

    Update 51.cpp

--- 51.cpp
+++ 51.cpp
@@ -7,15 +7,17 @@ int main()
 {
 
 	cout << "Hello , Welcome to Franks Carper Cleaning Service" << endl;;
-	cout << "\nHow many small  rooms would like cleaned? " << endl;
 
+
+	cout << "\nHow many small  rooms would like cleaned? " << endl;
 	int number_of_srooms;
 	cin >> number_of_srooms;
 
-	cout << "How many large  rooms would like cleaned? " << endl;
+
 
 	int number_of_lrooms;
 	cin >> number_of_lrooms;
+	cout << "How many large  rooms would like cleaned? " << endl;
 
 
 	const double price_per_sroom{ 25.0 };
@@ -24,10 +26,10 @@ int main()
 
 
 	const double sales_tax{ 0.06 };
-
-
-
 	const int estimate_expiry{ 30 };  // days
+
+
+
 
 	cout << "Estimate for carpet cleanig service" << endl;
 	cout << "Number of small rooms :" << number_of_srooms << endl;
@@ -38,11 +40,22 @@ int main()
 	
 	
 	
-	cout << "Cost: $" << (price_per_sroom * number_of_srooms) + (price_per_lroom * number_of_lrooms) << endl;
+	cout << "Cost: $" << (price_per_sroom * number_of_srooms) +
+		(price_per_lroom * number_of_lrooms)
+		<< endl;
 
-	cout << "Tax: $" << (price_per_sroom * number_of_srooms* sales_tax) + (price_per_lroom * number_of_lrooms * sales_tax) << endl;
+	cout << "Tax: $" << (price_per_sroom * number_of_srooms* sales_tax) +
+		(price_per_lroom * number_of_lrooms * sales_tax)
+		<< endl;
+
 	cout << "====================" << endl;
-	cout << "Totsal estimate : $" << (number_of_srooms * price_per_sroom) + (number_of_lrooms * price_per_lroom) + (price_per_sroom * number_of_srooms* sales_tax) + (price_per_lroom * number_of_lrooms * sales_tax) << endl;
+	cout << "Totsal estimate : $" << (number_of_srooms * price_per_sroom) +
+		(number_of_lrooms * price_per_lroom) + 
+		(price_per_sroom * number_of_srooms* sales_tax) + 
+		(price_per_lroom * number_of_lrooms * sales_tax) 
+		<< endl;
+
+
 	cout << "This estimate is valid for " << estimate_expiry << " days" << endl;
 
 
