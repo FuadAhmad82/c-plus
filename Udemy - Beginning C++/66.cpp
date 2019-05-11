5454514148885eaa4da01063d0a450c771c1a37f
FuadAhmad82 <lsfuad@hotmail.com>
2019-05-11 19:25:25 +0300

    Create 66.cpp

---
+++ Udemy - Beginning C++/66.cpp
@@ -0,0 +1,36 @@
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	
+	cout << "This is Program to change dollar to uero " << endl;
+
+
+
+	const double euro {1.6};
+	int doll ; 
+
+
+	cout << "Please Enter dollar " << endl; 
+
+	cin >> doll; 
+
+	cout << "this is your price : " << doll * euro << endl;
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
+
